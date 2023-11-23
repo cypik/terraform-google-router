5122db04003e11ac62c16dd515a7550fb4c1eb6b
opzsuresh <suresh.y@opz0.io>
2023-11-23 16:08:09 +0530

    update org name

--- example/interconnect_attachment/example.tf
+++ example/interconnect_attachment/example.tf
@@ -8,7 +8,7 @@ provider "google" {
 #### vpc module call.
 ####==============================================================================
 module "vpc" {
-  source                                    = "git::https://github.com/opz0/terraform-gcp-vpc.git?ref=v1.0.0"
+  source                                    = "git::https://github.com/cypik/terraform-gcp-vpc.git?ref=v1.0.0"
   name                                      = "app"
   environment                               = "test"
   network_firewall_policy_enforcement_order = "AFTER_CLASSIC_FIREWALL"
